86c4e435d52347cdfdc070d80c2db28c9208ec89
Lordbunny <624656776@qq.com>
2020-09-22 09:52:01 +0800

    commit

---
+++ src/components/inquireEditAdd.vue
@@ -0,0 +1,120 @@
+<template>
+  <div class="reqbox" v-show="reqbox">//formbox change to reqbox
+    <div class="req-box">
+      <div class="re-row">//flex change to re  
+        <div class="re-row">
+          <label for="project">项目</label>
+          <input type="text" id="project" v-model="reqboxmsg.project" />
+        </div>
+        <div class="re-row">
+          <label for="post">岗位</label>
+          <input type="text" id="post" v-model="reqboxmsg.post" />
+        </div>
+      </div>
+      <div class="re-row">
+        <label for="renum">需求人数</label>
+        <input type="text" id="renum" v-model="reqboxmsg.renum" />
+      </div>
+      <div class="re-row">
+        <label for="requestion">要求</label>
+        <textarea id="requestion" v-model="reqboxmsg.requestion"></textarea>
+      </div>
+      <div class="re-row">
+        <label for="priority">优先级</label>
+        <input type="text" id="priority" v-model="reqoxmsg.priority" />
+      </div>
+    
+
+      </div>
+      <div class="re-row btn">
+        <button @click="close">取消</button>
+        <button class="primary" @click="submit">保存</button>
+      </div>
+    </div>
+  <!--</div>-->
+</template>
+
+<script>
+export default {
+    name: 'inquireFormbox',
+    props: {
+        formbox: {
+            type: Number, // 0-关闭，1-编辑, 2-增加 
+            default: false
+        },
+        reqboxmsg: {
+            type: Object
+        }
+    },
+    methods: {
+        close: function(){
+            //console.log(this.formbox)
+            this.$emit("close", this.reqbox)
+        },
+        submit: function() {
+            let msg = {
+                'formbox': this.reqbox,
+                'formboxmsg': this.reqboxmsg
+            }
+            this.$emit("submit", msg)
+        }
+    }
+};
+</script>
+
+<style scoped>
+.formbox {
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(0, 0, 0, 0.3);
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999;
+}
+label {
+  font-weight: bold;
+  min-width: 120px;
+}
+textarea{
+   resize:none;
+   width: 490px;
+   height: 60px;
+}
+.formbox-box {
+  width: 50vw;
+  height: 70vh;
+  background-color: rgba(255, 255, 255, 1);
+  border: 1px solid;
+  padding: 20px;
+  display: flex;
+  flex-flow: column;
+  align-items: flex-start;
+  justify-content: space-around;
+}
+.flex-row {
+  display: flex;
+  flex-flow: row nowrap;
+  width: 100%
+}
+.btn {
+  justify-content: space-around;
+}
+button {
+  border-radius: 20px;
+  padding: 12px 23px;
+  white-space: nowrap;
+  cursor: pointer;
+  background: #fff;
+  border: 1px solid #dcdfe6;
+  width: 200px;
+}
+.primary {
+  color: #fff;
+  background-color: #409eff;
+  border-color: #409eff;
+}
+</style>
